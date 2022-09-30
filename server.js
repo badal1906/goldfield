09ba35ed651c0608b3e8f748c5423d540421ec80
Badal Patial <badal1906@gmail.com>
2022-09-30 11:22:57 +0530

    up

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const path = require("path");
-const dotenv = require("dotenv");
-// dotenv.config();
 
 app.use(express.json());
 
