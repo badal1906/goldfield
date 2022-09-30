b36fcc372e4f3d5912ddcd5eeffd8a1c7a25b764
Badal Patial <badal1906@gmail.com>
2022-09-30 11:05:11 +0530

    up

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ const contactRoute = require("./routes/contact");
 app.use("/file", downloadRoute);
 app.use("/user", contactRoute);
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log("server is running " + PORT);
